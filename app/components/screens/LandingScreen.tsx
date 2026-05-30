d6ce4f465ef37ab9b92c281931a29d0a274a1cc8
Ishaan Wadhwa <dr.ishaan@medevolv.in>
2026-05-30 07:54:56 +0530

    Fix TypeScript: add 'as const' to ease array

--- app/components/screens/LandingScreen.tsx
+++ app/components/screens/LandingScreen.tsx
@@ -26,7 +26,7 @@ const fadeInUp = {
   animate: { 
     opacity: 1, 
     y: 0,
-    transition: { duration: 0.4, ease: [0.16, 1, 0.3, 1] }
+    transition: { duration: 0.4, ease: [0.16, 1, 0.3, 1] as const }
   },
 };
 
--- dist/server/webpack-runtime.js
+++ dist/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b5b5cd48a874b538")
+/******/ 		__webpack_require__.h = () => ("9170c84f32fa63b6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
