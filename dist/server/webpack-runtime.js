963088ce1da4db496fc12cf067e1974a44dd82bb
Ishaan Wadhwa <dr.ishaan@medevolv.in>
2026-05-30 07:53:22 +0530

    Fix CSS: use standard transition timing, not custom classes in @apply

--- dist/server/webpack-runtime.js
+++ dist/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b117a4cc71c68ee6")
+/******/ 		__webpack_require__.h = () => ("b5b5cd48a874b538")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
